414995003ac41c80ce0f2098c5fa37a5fc5c92b8
shikhar9350sharma <shikharsharma593@gmail.com>
2025-07-16 15:21:58 +0530

    the border is now finished from the sectionOne components

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SectionOne = () => {
   return (
     <div>
-      <section className='w-full  h-full mx-auto border border-gray-800 mt-12 bg-[#f7f5ff] flex flex-col gap-4 items-center py-8'>
+      <section className='w-full  h-full mx-auto mt-12 bg-[#f7f5ff] flex flex-col gap-4 items-center py-8'>
         <div className='p-3' ><h1 className='text-6xl font-sans font-semibold text-[#372580]'>Secure email that protects your privacy</h1></div>
         <div className='p-3'><h3 className='font-sans text-gray-600 text-[18px]'>Keep your conversation private with Proton Mail, an encrypted email service based in Switzerland.</h3> </div>
         <div className='flex space-x-3  justify-center items-center w-full h-full'>
