c0313245a5e04d05467e432458a45fa0edc44682
shikhar9350sharma <shikharsharma593@gmail.com>
2025-07-20 22:35:32 +0530

    initial commit

--- src/components/SectionEleven.jsx
+++ src/components/SectionEleven.jsx
@@ -3,7 +3,107 @@ import React from 'react'
 const SectionEleven = () => {
   return (
     <div>
-      <section className='w-full min-h-screen flex items-center gap-2 py-4'></section>
+      <section className='w-full min-h-screen flex flex-col gap-2 p-10 bg-[#f7f5ff]'>
+        <div className='grid grid-cols-1 sm:grid-flow-row md:grid-cols-3 lg:grid-cols-5 gap-8 py-6 border-b border-gray-300'>
+          <div className='flex flex-col space-y-4 items-start'>
+            <div>
+              <span><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1703162849%2Fstatic%2Flogos%2Ftexts%2Fproton-purple_lf83vr.svg" alt="proton-img-22" /></span>
+              <span className='font-sans font-semibold text-[#372580]'>Privacy by default</span>
+            </div>
+            <div className='flex items-start flex-wrap gap-3'>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Fx.svg" alt="proton-img-23" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Freddit.svg" alt="proton-img-24" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Finstagram.svg" alt="proton-img-25" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Ffacebook.svg" alt="proton-img-26" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Flinkedin.svg" alt="proton-img-27" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Frss.svg" alt="proton-img-28" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Fthreads.svg" alt="proton-img-29" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Fwhatsapp.svg" alt="proton-img-30" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Ftelegram.svg" alt="proton-img-31" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Fuservoice.svg" alt="proton-img-32" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Fyoutube.svg" alt="proton-img-33" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Ftiktok.svg" alt="proton-img-34" /></button></span>
+              <span><button><img src="https://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fv1727943149%2Fstatic%2Fsocial%2Fsquare%2Fbluesky.svg" alt="proton-img-35" /></button></span>
+            </div>
+            <span className='font-sans text-gray-600 font-semibold'>Proton AG</span>
+            <span className='font-sans text-sm text-gray-600'>Route de la Galaise 32
+              1228 Plan-les-Ouates
+              Geneva, Switzerland</span>
+
+          </div>
+          <div className='flex flex-col space-y-6'>
+            <div className='font-sans font-semibold text-xl text-gray-600'>Products</div>
+            <ul className='flex flex-col space-y-4'>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Proton Mail</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Proton Calender</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Proton Drive</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Proton VPN</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Proton Pass</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Proton Wallet</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Proton for Business</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Switch to Proton</a></li>
+            </ul>
+          </div>
+          <div className='flex flex-col space-y-6'>
+            <div className='font-sans font-semibold text-xl text-gray-600'>Privacy and community</div>
+            <ul className='flex flex-col space-y-4'>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Password Generator</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Tor</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Community</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Open source</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Pricing</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Student discount</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Big Tech fines tracker</a></li>
+            </ul>
+          </div>
+          <div className='flex flex-col space-y-6'>
+            <div className='font-sans font-semibold text-xl text-gray-600'>Company</div>
+            <ul className='flex flex-col space-y-4'>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">About us</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Team</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Imapct</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Proton Foundation</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Careers</a><button className='font-sans text-white bg-[#6d4aff] rounded mx-2 px-2 text-sm'>We're hiring</button></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Shop</a></li>
+            </ul>
+          </div>
+          <div className='flex flex-col space-y-6'>
+            <div className='font-sans font-semibold text-xl text-gray-600'>Connect</div>
+            <ul className='flex flex-col space-y-4'>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Blog</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Help and support</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Partners and affiliates</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Press and media</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Contact us</a></li>
+              <li className='hover:underline text-[#372580] font-sans font-semibold'><a href="/">Donate</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className='flex items-center justify-between gap-4 m-4 p-4'>
+          <div>
+            <ul className='flex space-x-5 items-center'>
+              <li className='hover:underline text-[#372580] font-sans '><a href="/">System status</a></li>
+              <li className='hover:underline text-[#372580] font-sans '><a href="/">Report abuse</a></li>
+              <li className='hover:underline text-[#372580] font-sans '><a href="/">Report a problem</a></li>
+              <li className='hover:underline text-[#372580] font-sans '><a href="/">Report a security issue</a></li>  
+              <li className='hover:underline text-[#372580] font-sans '><a href="/">Request a feature</a></li>  
+            </ul>
+          </div>
+          <div><button className='flex items-center font-sans font-semibold bg-white text-[#6d4aff]  border-2 border-[#6d4aff]  rounded-xl px-4 text-xl hover:bg-[#6d4aff] hover:text-white'>English</button></div>
+        </div>
+        <div className='ml-10'>
+          <ul className='flex space-x-5 items-center text-sm text-gray-600'>
+            <li className='hover:underline font-sans'><a href="/">Privacy policy</a></li>
+            <li className='hover:underline font-sans'><a href="/">Terms & conditions</a></li>
+            <li className='hover:underline font-sans'><a href="/">Transparency report</a></li>
+            <li><span>&copy; 2025 Proton AG. All rights reserved.</span></li>
+          
+          </ul>
+        </div>
+         
+      
+        
+      </section>
     </div>
   )
 }
